004fafa0a2db5fbb57582430e57bb70b50fa3dbc
nicopatronelli <nicopatronelli@gmail.com>
2018-11-12 15:37:28 -0300

    Subo el proyecto

---
+++ SIEL_Superior_2/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SIEL_Superior_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbx1 = new System.Windows.Forms.TextBox();
+            this.btnNormaInf = new System.Windows.Forms.Button();
+            this.lblNorma = new System.Windows.Forms.Label();
+            this.btnNorma1 = new System.Windows.Forms.Button();
+            this.btnNorma2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMDD = new System.Windows.Forms.Button();
+            this.lblMDD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbx1
+            // 
+            this.txtbx1.Location = new System.Drawing.Point(28, 26);
+            this.txtbx1.Multiline = true;
+            this.txtbx1.Name = "txtbx1";
+            this.txtbx1.Size = new System.Drawing.Size(401, 127);
+            this.txtbx1.TabIndex = 0;
+            // 
+            // btnNormaInf
+            // 
+            this.btnNormaInf.Location = new System.Drawing.Point(458, 26);
+            this.btnNormaInf.Name = "btnNormaInf";
+            this.btnNormaInf.Size = new System.Drawing.Size(90, 23);
+            this.btnNormaInf.TabIndex = 1;
+            this.btnNormaInf.TabStop = false;
+            this.btnNormaInf.Text = "Norma Infinito";
+            this.btnNormaInf.UseVisualStyleBackColor = true;
+            this.btnNormaInf.Click += new System.EventHandler(this.btnNormaInf_Click);
+            // 
+            // lblNorma
+            // 
+            this.lblNorma.AutoSize = true;
+            this.lblNorma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNorma.Location = new System.Drawing.Point(117, 164);
+            this.lblNorma.Name = "lblNorma";
+            this.lblNorma.Size = new System.Drawing.Size(66, 24);
+            this.lblNorma.TabIndex = 2;
+            this.lblNorma.Text = "label1";
+            // 
+            // btnNorma1
+            // 
+            this.btnNorma1.Location = new System.Drawing.Point(458, 70);
+            this.btnNorma1.Name = "btnNorma1";
+            this.btnNorma1.Size = new System.Drawing.Size(90, 23);
+            this.btnNorma1.TabIndex = 4;
+            this.btnNorma1.TabStop = false;
+            this.btnNorma1.Text = "Norma 1";
+            this.btnNorma1.UseVisualStyleBackColor = true;
+            this.btnNorma1.Click += new System.EventHandler(this.btnNorma1_Click);
+            // 
+            // btnNorma2
+            // 
+            this.btnNorma2.Location = new System.Drawing.Point(458, 116);
+            this.btnNorma2.Name = "btnNorma2";
+            this.btnNorma2.Size = new System.Drawing.Size(90, 23);
+            this.btnNorma2.TabIndex = 5;
+            this.btnNorma2.TabStop = false;
+            this.btnNorma2.Text = "Norma 2";
+            this.btnNorma2.UseVisualStyleBackColor = true;
+            this.btnNorma2.Click += new System.EventHandler(this.btnNorma2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Resultado:";
+            // 
+            // btnMDD
+            // 
+            this.btnMDD.Location = new System.Drawing.Point(28, 205);
+            this.btnMDD.Name = "btnMDD";
+            this.btnMDD.Size = new System.Drawing.Size(242, 23);
+            this.btnMDD.TabIndex = 7;
+            this.btnMDD.Text = "Verificar Matriz Diagonalmente Dominante";
+            this.btnMDD.UseVisualStyleBackColor = true;
+            this.btnMDD.Click += new System.EventHandler(this.btnMDD_Click);
+            // 
+            // lblMDD
+            // 
+            this.lblMDD.AutoSize = true;
+            this.lblMDD.Location = new System.Drawing.Point(326, 205);
+            this.lblMDD.Name = "lblMDD";
+            this.lblMDD.Size = new System.Drawing.Size(35, 13);
+            this.lblMDD.TabIndex = 8;
+            this.lblMDD.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 366);
+            this.Controls.Add(this.lblMDD);
+            this.Controls.Add(this.btnMDD);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNorma2);
+            this.Controls.Add(this.btnNorma1);
+            this.Controls.Add(this.lblNorma);
+            this.Controls.Add(this.btnNormaInf);
+            this.Controls.Add(this.txtbx1);
+            this.Name = "Form1";
+            this.Text = "SIEL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbx1;
+        private System.Windows.Forms.Button btnNormaInf;
+        private System.Windows.Forms.Label lblNorma;
+        private System.Windows.Forms.Button btnNorma1;
+        private System.Windows.Forms.Button btnNorma2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMDD;
+        private System.Windows.Forms.Label lblMDD;
+    }
+}
+
---
+++ SIEL_Superior_2/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Math;
+
+namespace SIEL_Superior_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormaInf_Click(object sender, EventArgs e)
+        {
+            double[,] coeficientes = obtenerCoeficientesYTerminosIndependientes();
+            int orden = obtenerOrden();
+            List<double> listaSumaFilas = new List<double>();
+
+            for (int fila = 1; fila <= orden; fila++ )
+            {
+                // Una vez que estoy parado en una fila...
+                double sumaFilaActual = 0;
+                // Hago columna <= orden para NO considerar la columna de t.i.
+                for (int columna = 1; columna <= orden; columna++ ) 
+                {
+                    sumaFilaActual = sumaFilaActual + Math.Abs(coeficientes[fila, columna]);
+                }
+                listaSumaFilas.Add(sumaFilaActual);
+            }
+
+            double normaInfinito = listaSumaFilas.Max();
+
+            lblNorma.Text = normaInfinito.ToString();
+
+        } // Fin btn_Click() 
+
+        private void btnNorma1_Click(object sender, EventArgs e)
+        {
+            double[,] coeficientes = obtenerCoeficientesYTerminosIndependientes();
+            int orden = obtenerOrden();
+            List<double> listaSumaColumnas = new List<double>();
+
+            // Hago columna <= orden para NO considerar la columna de t.i.
+            for (int columna = 1; columna <= orden; columna++)
+            {
+                // Una vez que estoy parado en una columna...
+                double sumaColumnaActual = 0;
+                for (int fila = 1; fila <= orden; fila++)
+                {
+                    sumaColumnaActual = sumaColumnaActual + Math.Abs(coeficientes[fila, columna]);
+                }
+                listaSumaColumnas.Add(sumaColumnaActual);
+            }
+
+            double norma1 = listaSumaColumnas.Max();
+
+            lblNorma.Text = norma1.ToString();
+        }
+
+        private void btnNorma2_Click(object sender, EventArgs e)
+        {   
+            double[,] coeficientesSolos = obtenerCoeficientesSolos();
+            double norma2 = Accord.Math.Norm.Norm2(coeficientesSolos);
+            lblNorma.Text = norma2.ToString();
+        }
+
+        private void btnMDD_Click(object sender, EventArgs e)
+        {
+            double[,] coeficientes = obtenerCoeficientesSolos();
+            int orden = obtenerOrden();
+            // Por cada fila
+            int columnaDP = 0;
+            double valorDP = 0;
+            double sumaFila = 0;
+            bool flagMDD = true; // Comenzamos suponiendo que la matriz es diagonalmente dominante
+            bool flagED = true; // Comenzamos suponiendo que la matriz es estrictamente dominante
+            for (int fila = 0; fila < orden; fila++)
+            {   
+                // Recorro todas las columnas
+                for (int columna = 0; columna < orden; columna++)
+                {
+                    // Sumo todos los elementos de la fila actual
+                    sumaFila = sumaFila + Math.Abs(coeficientes[fila, columna]); 
+                }
+                valorDP = Math.Abs(coeficientes[fila, columnaDP]);
+                sumaFila = sumaFila - valorDP; // Como lo sume lo tengo que restar
+                if (!(valorDP > sumaFila)) // estrictamente dominante 
+                {
+                    // La matriz ya no puede ser estrictamente dominante, pero puede ser
+                    // diagonalmente dominante aún
+                    flagED = false; 
+                }
+                if (!(valorDP >= sumaFila)) // diagonalmente dominante
+                {
+                    // La matriz ya no puede ser diagonalmente dominante 
+                    flagMDD = false;
+                    break;
+                }
+                columnaDP++;
+                sumaFila = 0;
+            } // Fin for filas 
+
+            // Si llego a este punto significa que la matriz es diagonalmente dominante
+            if (flagMDD == true && flagED == true)
+            {
+                lblMDD.Text = "La matriz de coeficientes asociada al SEL ingresada es estrictamente diagonal dominante";
+            }
+            else if (flagMDD == true && flagED == false)
+            {
+                lblMDD.Text = "La matriz de coeficientes asociada al SEL ingresada es diagonalmente dominante";
+            }
+            else
+            {
+                lblMDD.Text = "La matriz de coeficientes asociada al SEL ingresado NO es diagonalmente dominante";
+            }
+         } // Fin btnMDD_Click
+
+        /*
+         * MÉTODOS AUXILIARES  
+        */
+
+        private double[,] matrizTranspuesta(double[,] matriz)
+        {
+            int orden = obtenerOrden();
+            double [,] resultado = new double[orden+1, orden+1];
+
+            for (int fila = 1; fila <= orden; fila++ )
+            {
+                for (int columna = 1; columna <= orden; columna++)
+                {
+                    resultado[fila, columna] = matriz[columna, fila];
+                }
+            }
+
+        return resultado;
+        } // Fin matrizTranspuesta()
+
+        private int obtenerOrden()
+        {
+            string[] ecuaciones = txtbx1.Text.Split(new[] { "\n" },
+                StringSplitOptions.None);
+
+            return ecuaciones.Length; 
+        }
+
+        private double[,] obtenerCoeficientesYTerminosIndependientes()
+        {
+            string sistemaDeEcuaciones;
+            sistemaDeEcuaciones = txtbx1.Text;
+
+            string[] ecuaciones = sistemaDeEcuaciones.Split(new[] { "\n" },
+                StringSplitOptions.None);
+            //string[] ecuaciones = sistemaDeEcuaciones.Split(';');
+            int orden = obtenerOrden();
+
+            // Reemplazo los x_nn por ; 
+            for (int i = 0; i < orden; i++)
+            {
+                //ecuaciones[i] = ecuaciones[i].Replace(Regex., ";");
+                ecuaciones[i] = Regex.Replace(ecuaciones[i], @"x_\d*", ";");
+            }
+
+            // Eliminamos los espacios en blanco y reemplazamos el = por ""
+            for (int i = 0; i < orden; i++)
+            {
+                ecuaciones[i] = ecuaciones[i].Replace(" ", "");
+                ecuaciones[i] = ecuaciones[i].Replace("=", "");
+            }
+
+            // En este punto tenemos en ecuaciones por ejemplo: 2;+3;5 
+            double[,] coeficientes = new double[orden + 1, orden + 2];
+            for (int i = 0; i < orden; i++)
+            {
+                string[] coef = ecuaciones[i].Split(';');
+                for (int j = 0; j < orden + 1; j++) // orden + 1 por el t.i.
+                {
+                    coeficientes[i + 1, j + 1] = Convert.ToDouble(coef[j]);
+                }
+            }
+
+            return coeficientes;
+
+        } // Fin obtenerCoeficientes()
+
+        private double[,] obtenerCoeficientesSolos()
+        {
+            string sistemaDeEcuaciones;
+            sistemaDeEcuaciones = txtbx1.Text;
+
+            string[] ecuaciones = sistemaDeEcuaciones.Split(new[] { "\n" },
+                StringSplitOptions.None);
+
+            int orden = obtenerOrden();
+
+            // Reemplazo los x_nn por ; 
+            for (int i = 0; i < orden; i++)
+            {
+                //ecuaciones[i] = ecuaciones[i].Replace(Regex., ";");
+                ecuaciones[i] = Regex.Replace(ecuaciones[i], @"x_\d*", ";");
+            }
+
+            // Eliminamos los espacios en blanco y reemplazamos el = por ""
+            for (int i = 0; i < orden; i++)
+            {
+                ecuaciones[i] = ecuaciones[i].Replace(" ", "");
+                ecuaciones[i] = ecuaciones[i].Replace("=", "");
+            }
+
+            // En este punto tenemos en ecuaciones por ejemplo: 2;+3;5 
+            double[,] coeficientes = new double[orden, orden];
+            for (int i = 0; i < orden; i++)
+            {
+                string[] coef = ecuaciones[i].Split(';');
+                for (int j = 0; j < orden; j++) 
+                {
+                    coeficientes[i, j] = Convert.ToDouble(coef[j]);
+                }
+            }
+
+            return coeficientes;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblNorma.Text = "";
+        }
+
+    } // Fin public partial class Form 1
+
+}
